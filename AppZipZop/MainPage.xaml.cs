370c4d272963ef7321b5fd45b7a7a21405468719
Thiago Cristiano Tavares <thiago.tavares@lais.huol.ufrn.br>
2016-12-13 10:22:37 -0300

    Windows Phone iniciado

---
+++ AppZipZop/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using AppZipZop.Resources;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace AppZipZop
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private string ip = "http://10.21.0.137";
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            getUsuarios();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Notificação recebida com o aplicativo fechado
+            // Este método é chamado e os dados vem na QueryString
+            var dic = NavigationContext.QueryString;
+            // Atualiza lista de mensagens
+            if (dic.ContainsKey("Msg1")) listMsg.Items.Add(dic["Msg1"]);
+            if (dic.ContainsKey("Msg2")) listMsg.Items.Add(dic["Msg2"]);
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+
+
+        private Models.Usuario usuario;
+
+        List<Models.Usuario> usuarios = new List<Models.Usuario>();
+
+        public async void getUsuarios()
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(ip);
+
+            var response = await httpClient.GetAsync("/20131011110029/api/usuuario");
+            var str = response.Content.ReadAsStringAsync().Result;
+            List<Models.Usuario> obj = JsonConvert.DeserializeObject<List<Models.Usuario>>(str);
+            usuarios = obj;
+        }
+    }
+}
---
+++ AppZipZop/PageLogin.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+using Microsoft.Phone.Notification;
+using System.IO.IsolatedStorage;
+
+namespace AppZipZop
+{
+    public partial class PageLogin : PhoneApplicationPage
+    {
+        private string ip = "http://10.21.0.137/";
+
+        public PageLogin()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            Models.Usuario usuario = new Models.Usuario
+            {
+                Nome = txbNome.Text
+            };
+
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(ip);
+            string s = JsonConvert.SerializeObject(usuario);
+
+            var content = new StringContent(s, Encoding.UTF8, "application/json");
+            await httpClient.PostAsync("/20131011110029/api/usuario", content);
+            MessageBox.Show("Usuário criado");
+
+            Dispatcher.BeginInvoke(() => {
+                var settings = IsolatedStorageSettings.ApplicationSettings;
+                settings.Add("Nome", usuario.Nome);
+                settings.Save();
+
+                NavigationService.Navigate(new Uri("/MainPage.xaml?registered=true", UriKind.Relative));
+            });
+        }
+    }
+}
